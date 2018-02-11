9a81d642fec636bd503f024fdba3a398e11440b9
Ruofei Shen <shenruofei@outlook.com>
2018-02-11 02:29:48 -0800

    implement LRU & pass first three tets

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -13,7 +13,16 @@ struct Set {
     bool valid;
     int min;
     int max;
+    int seq;
 };
+
+void postProcess(struct Set sets[], int n, int idx) {
+	for(int i=0; i<n; ++i) {
+		if(i != idx) {
+			sets[i].seq += 1;
+		}
+	}
+}
 
 int main(int argc, char** argv) {
     int s = 0;
@@ -48,10 +57,12 @@ int main(int argc, char** argv) {
     printf("%d%d%d\n",s, E, b);
     FILE *pFile; /* pointer to FILE object */
     printf("%s\n", t);
-    struct Set sets[s][E];
-    for(int i=0; i<s; ++i) {
+    int S = 0x1 << s;
+    struct Set sets[S][E];
+    for(int i=0; i<S; ++i) {
         for(int j=0; j<E; ++j) {
             sets[i][j].valid = false;
+            sets[i][j].seq = -1;
         }
     }
     pFile = fopen(t,"r"); /* open trace file for reading */
@@ -72,95 +83,119 @@ int main(int argc, char** argv) {
         int op_tag = (address >> (b + s)) & temp3;
         int op_min = address & 0xfffffff0;
         int op_max = address | 0x0000000f;
-        printf("%d, %d, %d, %d\n", op_idx, op_tag, op_min, op_max);
+        // printf("%d, %d, %d, %d\n", op_idx, op_tag, op_min, op_max);
+        bool flag = false;
+    	int temp_idx = -1;
+    	int temp_seq = -1;
         if (access_type == 'M') {
-        	if(!sets[op_idx][0].valid) {
-                miss += 1;
-                hit += 1;
-                sets[op_idx][0].valid = true;
-                sets[op_idx][0].tag = op_tag;
-                sets[op_idx][0].min = op_min;
-		        sets[op_idx][0].max = op_max;
-            }else {
-                if(sets[op_idx][0].tag == op_tag) {
-                    if(address <= sets[op_idx][0].max && address >= sets[op_idx][0].min){
-                		hit += 2;
-                	}else {
-                		miss += 1;
-                		hit += 1;
-                		eviction += 1;
-		                sets[op_idx][0].valid = true;
-		                sets[op_idx][0].tag = op_tag;
-		                sets[op_idx][0].min = op_min;
-		                sets[op_idx][0].max = op_max;
-                	}
-                }else {
-                    miss += 1;
-                    hit += 1;
-                    eviction += 1;
-                    sets[op_idx][0].valid = true;
-	                sets[op_idx][0].tag = op_tag;
-	                sets[op_idx][0].min = op_min;
-	                sets[op_idx][0].max = op_max;
-                }
-            }
-        }else if(access_type == 'L') {
-            if(!sets[op_idx][0].valid) {
-                miss += 1;
-                sets[op_idx][0].valid = true;
-                sets[op_idx][0].tag = op_tag;
-                sets[op_idx][0].min = op_min;
-                sets[op_idx][0].max = op_max;
-            }else {
-                if(sets[op_idx][0].tag == op_tag) {
-                	if(address <= sets[op_idx][0].max && address >= sets[op_idx][0].min){
-                		hit += 1;
-                	}else {
-                		miss += 1;
-		                sets[op_idx][0].valid = true;
-		                sets[op_idx][0].tag = op_tag;
-		                sets[op_idx][0].min = op_min;
-		                sets[op_idx][0].max = op_max;
-                	}
-                }else {
-                    miss += 1;
-                    eviction += 1;
-                    sets[op_idx][0].valid = true;
-	                sets[op_idx][0].tag = op_tag;
-	                sets[op_idx][0].min = op_min;
-	                sets[op_idx][0].max = op_max;
-                }
-            }
-        } else {
-        	if(!sets[op_idx][0].valid) {
-        		sets[op_idx][0].valid = true;
-        		sets[op_idx][0].tag = op_tag;
-        		sets[op_idx][0].min = op_min;
-		        sets[op_idx][0].max = op_max;
-        		miss += 1;
-        		// hit += 1;
-        	}else {
-        		if(sets[op_idx][0].tag == op_tag){
-        			if(address <= sets[op_idx][0].max && address >= sets[op_idx][0].min){
-                		hit += 1;
-                	}else {
-                		miss += 1;
-		                sets[op_idx][0].valid = true;
-		                sets[op_idx][0].tag = op_tag;
-		                sets[op_idx][0].min = op_min;
-		                sets[op_idx][0].max = op_max;
-                	}
-        		}else{
-        			miss += 1;
-        			eviction += 1;
-        			sets[op_idx][0].valid = true;
-	                sets[op_idx][0].tag = op_tag;
-	                sets[op_idx][0].min = op_min;
-	                sets[op_idx][0].max = op_max;
+        	for(int j=0; j<E; ++j) {
+        		if(sets[op_idx][j].valid && sets[op_idx][j].tag == op_tag && address <= sets[op_idx][j].max && address >= sets[op_idx][j].min){
+        			hit += 2;
+        			sets[op_idx][j].seq = 0;
+        			temp_idx = j;
+        			flag = true;
+        			break;
+        		} else if(!sets[op_idx][j].valid){
+        			temp_idx = j;
+        			//temp_seq = 0;
+        			break;
+        		} else {
+        			if(temp_seq < sets[op_idx][j].seq){
+        				temp_seq = sets[op_idx][j].seq;
+        				temp_idx = j;
+        			}
         		}
         	}
+        	if(!flag){
+        		if(!sets[op_idx][temp_idx].valid) {
+        			miss += 1;
+        			hit += 1;
+        			sets[op_idx][temp_idx].valid = true;
+        			sets[op_idx][temp_idx].seq = 0;
+        			sets[op_idx][temp_idx].tag = op_tag;
+        			sets[op_idx][temp_idx].min = op_min;
+        			sets[op_idx][temp_idx].max = op_max;
+        		}else {
+        			miss += 1;
+        			hit += 1;
+        			eviction += 1;
+        			sets[op_idx][temp_idx].seq = 0;
+        			sets[op_idx][temp_idx].tag = op_tag;
+        			sets[op_idx][temp_idx].min = op_min;
+        			sets[op_idx][temp_idx].max = op_max;
+        		}
+        	}
+        	postProcess(sets[op_idx], E, temp_idx);
+        	// if(!sets[op_idx][0].valid) {
+         //        miss += 1;
+         //        hit += 1;
+         //        sets[op_idx][0].valid = true;
+         //        sets[op_idx][0].tag = op_tag;
+         //        sets[op_idx][0].min = op_min;
+		       //  sets[op_idx][0].max = op_max;
+         //    }else {
+         //        if(sets[op_idx][0].tag == op_tag) {
+         //            if(address <= sets[op_idx][0].max && address >= sets[op_idx][0].min){
+         //        		hit += 2;
+         //        	}else {
+         //        		miss += 1;
+         //        		hit += 1;
+         //        		eviction += 1;
+		       //          sets[op_idx][0].tag = op_tag;
+		       //          sets[op_idx][0].min = op_min;
+		       //          sets[op_idx][0].max = op_max;
+         //        	}
+         //        }else {
+         //            miss += 1;
+         //            hit += 1;
+         //            eviction += 1;
+         //            sets[op_idx][0].valid = true;
+	        //         sets[op_idx][0].tag = op_tag;
+	        //         sets[op_idx][0].min = op_min;
+	        //         sets[op_idx][0].max = op_max;
+         //        }
+            //}
+        }else if(access_type == 'L' || access_type == 'S') {
+        	for(int j=0; j<E; ++j) {
+        		if(sets[op_idx][j].valid && sets[op_idx][j].tag == op_tag && address <= sets[op_idx][j].max && address >= sets[op_idx][j].min){
+        			hit += 1;
+        			sets[op_idx][j].seq = 0;
+        			temp_idx = j;
+        			flag = true;
+        			break;
+        		} else if(!sets[op_idx][j].valid){
+        			//temp_seq = 0;
+        			temp_idx = j;
+        			break;
+        		} else {
+        			if(temp_seq < sets[op_idx][j].seq){
+        				temp_seq = sets[op_idx][j].seq;
+        				temp_idx = j;
+        			}
+        		}
+        	}
+        	if(!flag){
+        		if(!sets[op_idx][temp_idx].valid) {
+        			miss += 1;
+        			sets[op_idx][temp_idx].valid = true;
+        			sets[op_idx][temp_idx].seq = 0;
+        			sets[op_idx][temp_idx].tag = op_tag;
+        			sets[op_idx][temp_idx].min = op_min;
+        			sets[op_idx][temp_idx].max = op_max;
+        		}else {
+        			miss += 1;
+        			eviction += 1;
+        			sets[op_idx][temp_idx].seq = 0;
+        			sets[op_idx][temp_idx].tag = op_tag;
+        			sets[op_idx][temp_idx].min = op_min;
+        			sets[op_idx][temp_idx].max = op_max;
+        		}
+        	}
+        	postProcess(sets[op_idx], E, temp_idx);
         }
+        printf("%d index, %d subset, %d, %d, %d, \n", op_idx, temp_idx, op_tag, op_min, op_max);
         printSummary(hit, miss, eviction);
+        printf("\n");
        // printf("%c", access_type);
        // printf("%lx", address);
        // printf("%d", size);
